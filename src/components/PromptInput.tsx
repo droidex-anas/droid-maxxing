ebc1a94cf8762ae953de42bc60f8f2ca07240151
Anas <anasibnanwar1@gmail.com>
2026-09-11 14:11:15 +0530

    feat(composer): show agent processes in a toolbar chip

--- src/components/PromptInput.tsx
+++ src/components/PromptInput.tsx
@@ -35,6 +35,7 @@ import { useFileAttachments } from '../hooks/useFileAttachments';
 import { useComposerFileDrop } from '../hooks/useComposerFileDrop';
 import { ImageChip } from './composer/ImageChip';
 import { FileChip } from './composer/FileChip';
+import { RunningProcessesChip } from './composer/RunningProcessesChip';
 import { ImageViewerModal } from './composer/ImageViewerModal';
 import { ImageLightbox } from './media/ImageLightbox';
 import { imageSrc, partitionImagePaths } from '../lib/localImage';
@@ -1721,6 +1722,8 @@ export default function PromptInput({
             >
               <span>{isSpecMode ? 'Spec' : 'Chat'}</span>
             </button>
+
+            {activeSession && <RunningProcessesChip appSessionId={activeSession.appSessionId} />}
 
             <div className="flex-1 min-w-0" />
 
